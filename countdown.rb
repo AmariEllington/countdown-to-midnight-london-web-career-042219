1d5e21290a33a8e298da46b05d8cc2b4e8bd5c9d
Amari Ellington <amari.l.ellington@gmail.com>
2019-04-10 19:05:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown
   number = 0
     while number <= 10
       puts "#{number} SECOND(S)!"
-    number += 1
+    number -= 10
 end
   puts "HAPPY NEW YEAR!"
 end
@@ -15,7 +15,7 @@ def countdown_with_sleep
   number = 0
     while number <= 10
       puts "#{number} SECOND(S)!"
-    number += 1
+    number -= 10
     sleep(1)
 end
   puts "HAPPY NEW YEAR!"
